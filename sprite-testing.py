1733c8ca302104a80e68b1be1ea19b754930bfef
AkioR2028 <231078577+AkioR2028@users.noreply.github.com>
2026-05-06 21:08:49 +0000

    Wed, May 6, 2026, 2:08 PM -07:00

--- sprite-testing.py
+++ sprite-testing.py
@@ -2,21 +2,21 @@ import random
 import arcade
 
 # --- Constants ---
-SPRITE_SCALING_PLAYER = 0.03
-SPRITE_SCALING_COIN = 0.3
+SPRITE_SCALING_PLAYER = 0.3
+SPRITE_SCALING_COIN = 0.5
 COIN_COUNT = 50
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
-WINDOW_TITLE = "Coin Variants + Freeze Game"
+WINDOW_TITLE = "Working Coin Variants (No Errors)"
 
 
-# --- Coin Variants ---
+# Built-in Arcade textures (these ALWAYS exist)
 COIN_TYPES = [
-    {"texture": "coin_slow.png", "speed": 1},
-    {"texture": "coin_medium.png", "speed": 2},
-    {"texture": "coin_fast.png", "speed": 3},
-    {"texture": "coin_insane.png", "speed": 5},
+    {"texture": ":resources:images/items/coinGold.png", "speed": 1},
+    {"texture": ":resources:images/items/gemBlue.png", "speed": 2},
+    {"texture": ":resources:images/items/gemRed.png", "speed": 3},
+    {"texture": ":resources:images/items/star.png", "speed": 5},
 ]
 
 
@@ -25,10 +25,10 @@ class Coin(arcade.Sprite):
         super().__init__(texture, scale=scale)
 
         self.speed = speed
-        self.pick_new_direction()
         self.change_timer = random.uniform(1.0, 3.0)
+        self.pick_direction()
 
-    def pick_new_direction(self):
+    def pick_direction(self):
         direction = random.choice(["up", "down", "left", "right"])
 
         if direction == "up":
@@ -40,7 +40,7 @@ class Coin(arcade.Sprite):
         elif direction == "left":
             self.change_x = -self.speed
             self.change_y = 0
-        elif direction == "right":
+        else:  # right
             self.change_x = self.speed
             self.change_y = 0
 
@@ -48,16 +48,16 @@ class Coin(arcade.Sprite):
         self.center_x += self.change_x
         self.center_y += self.change_y
 
-        # Bounce off edges
+        # Bounce off walls
         if self.left < 0 or self.right > WINDOW_WIDTH:
             self.change_x *= -1
         if self.bottom < 0 or self.top > WINDOW_HEIGHT:
             self.change_y *= -1
 
-        # Change direction occasionally
+        # Occasionally change direction
         self.change_timer -= delta_time
         if self.change_timer <= 0:
-            self.pick_new_direction()
+            self.pick_direction()
             self.change_timer = random.uniform(1.0, 3.0)
 
 
@@ -65,11 +65,9 @@ class GameView(arcade.View):
     def __init__(self):
         super().__init__()
 
-        self.player_sprite = None
         self.player_list = None
         self.coin_list = None
-
-        self.background = None
+        self.player_sprite = None
 
         self.target_x = 0
         self.target_y = 0
@@ -80,20 +78,19 @@ class GameView(arcade.View):
         self.player_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
 
-        self.background = arcade.load_texture("bgimage.png")
-
-        # Player
+        # Player (built-in image)
         self.player_sprite = arcade.Sprite(
-            "waymo.avif",
+            ":resources:images/animated_characters/female_person/femalePerson_idle.png",
             scale=SPRITE_SCALING_PLAYER
         )
-        self.player_sprite.position = (50, 50)
+        self.player_sprite.center_x = 100
+        self.player_sprite.center_y = 100
         self.player_list.append(self.player_sprite)
 
-        self.target_x = 50
-        self.target_y = 50
+        self.target_x = 100
+        self.target_y = 100
 
-        # --- Create coins with variants ---
+        # Coins (variants)
         for _ in range(COIN_COUNT):
             coin_type = random.choice(COIN_TYPES)
 
@@ -110,16 +107,6 @@ class GameView(arcade.View):
 
     def on_draw(self):
         self.clear()
-
-        arcade.draw_texture_rect(
-            self.background,
-            arcade.rect.XYWH(
-                WINDOW_WIDTH / 2,
-                WINDOW_HEIGHT / 2,
-                WINDOW_WIDTH,
-                WINDOW_HEIGHT
-            )
-        )
 
         self.coin_list.draw()
         self.player_list.draw()
@@ -144,7 +131,7 @@ class GameView(arcade.View):
 
         self.coin_list.update(delta_time)
 
-        # Player movement
+        # Smooth movement
         speed = 5
         dx = self.target_x - self.player_sprite.center_x
         dy = self.target_y - self.player_sprite.center_y
@@ -162,6 +149,12 @@ class GameView(arcade.View):
             self.player_sprite, self.coin_list
         ):
             self.game_over = True
+
+    def on_key_press(self, key, modifiers):
+        # Press R to restart
+        if key == arcade.key.R:
+            self.setup()
+            self.game_over = False
 
 
 def main():
